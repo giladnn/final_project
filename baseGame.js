7f436e9e1d2a6dbaa608aab88e37ad9503ba2f8d
unknown <gilad.neimann@gmail.com>
2017-12-22 14:36:26 +0200

    add icons to random places

--- baseGame.js
+++ baseGame.js
@@ -1,11 +1,53 @@
 var theThing = document.querySelector("#thing");
 var container = document.querySelector("#contentContainer");
+if(container){
+	container.addEventListener("click", function(event) {
+		var xPosition = event.clientX - container.getBoundingClientRect().left - (theThing.clientWidth / 2);
+		var yPosition = event.clientY - container.getBoundingClientRect().top - (theThing.clientHeight / 2);
+		// in case of a wide border, the boarder-width needs to be considered in the formula above
+		theThing.style.left = xPosition + "px";
+		theThing.style.top = yPosition + "px";
+		}
+	);
+}
 
-container.addEventListener("click", function(event) {
-	var xPosition = event.clientX - container.getBoundingClientRect().left - (theThing.clientWidth / 2);
-	var yPosition = event.clientY - container.getBoundingClientRect().top - (theThing.clientHeight / 2);
-	// in case of a wide border, the boarder-width needs to be considered in the formula above
-	theThing.style.left = xPosition + "px";
-	theThing.style.top = yPosition + "px";
-	}
-);
+(function makeDiv(){
+	var divsize = ((Math.random()*100) + 50).toFixed();
+	var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+	$newdiv = $('<div/>').css({
+		'width':divsize+'px',
+		'height':divsize+'px',
+		'background-color': color
+	});
+	
+	var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
+	var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
+	$('body').append($newdiv.css({
+		'position':'absolute',
+		'left':posx+'px',
+		'top':posy+'px',
+		'display':'none'
+	}))
+	
+	// .fadeIn(100).delay(300).fadeOut(200, function(){
+	// 	$(this).remove();
+	// 	makeDiv(); 
+	//  }); ;
+
+	$newdiv
+		.fadeIn(100).delay(300).fadeOut(200, function(){
+			$(this).remove();
+			makeDiv(); 
+		 }); ;
+	// $newdiv.css({
+	// 	'position':'absolute',
+	// 	'left':posx+'px',
+	// 	'top':posy+'px',
+	// 	'display':'none'
+	// }).appendTo( 'body' ).fadeIn(100).delay(300).fadeOut(200, function(){
+	//    $(this).remove();
+	//    makeDiv(); 
+	// }); 
+})();
+
+
